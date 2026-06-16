bfd4e772827e95407a7a578721949fc88454fcb3
Bernadette-23 <lafagebernadette23@gmail.com>
2026-06-16 09:49:22 +0200

    maj mqtt

--- test_mqtt.py
+++ test_mqtt.py
@@ -1,4 +1,13 @@
 import paho.mqtt.client as mqtt
+import mariadb
+
+connexion = mariadb.connect(
+    host="192.168.1.10",
+    user="root",
+    password="motdepasse",
+    database="sae204"
+)
+
 
 # Adresse du broker
 broker = "broker.hivemq.com"
@@ -8,7 +17,14 @@ topic = "IUT/Colmar2026/SAE2.04/Maison1"
 
 # Fonction exécutée lorsqu'un message arrive
 def reception(client, userdata, message):
-    print(message.payload.decode())
+    texte = message.payload.decode()
+
+    print(texte)
+
+    fichier = open("donnees.csv", "a")
+    fichier.write(texte + "\n")
+    fichier.close()
+
 
 # Création du client MQTT
 client = mqtt.Client()
@@ -26,3 +42,4 @@ print("En attente des messages...")
 
 # Boucle infinie
 client.loop_forever()
+
