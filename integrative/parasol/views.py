7824766a0144d40c48370b89309c38be2390b56d
Bernadette-23 <lafagebernadette23@gmail.com>
2026-06-18 17:03:47 +0200

    maj csv

--- integrative/parasol/views.py
+++ integrative/parasol/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Capteur
 
 # Create your views here.
+
+def modifier_capteur(request, id):
+
+    capteur = Capteur.objects.get(id=id)
+
+    if request.method == "POST":
+
+        capteur.nom_capteur = request.POST["nom_capteur"]
+        capteur.emplacement = request.POST["emplacement"]
+        capteur.save()
+
+        return redirect("/")
+
+    return render(
+        request,
+        "capteurs/modifier_capteur.html",
+        {"capteur": capteur}
+    )
